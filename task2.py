2230b95a2b7f40068cc8bfdec18ad0d0c716ed02
emerson-bl <emerson.cook457@students.cmcss.net>
2025-12-17 14:15:07 -0600

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,1 +1,15 @@
+# Week 1: Chores and first purchase
+allowance += 4
+allowance += 10
+allowance -= 5
 
+# Week 2: Bonus week and purchase
+allowance *= 3
+allowance += 7
+allowance -= 8
+
+# Week 3: Savings
+allowance /= 2
+
+# Print final allowance
+print(f"Allowance: ${allowance}")
